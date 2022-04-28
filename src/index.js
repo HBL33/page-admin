c68e53c0aad177c619965fdf88279f1c9bd77b00
33011-49-03 <33011-49-03@exchange.ad.afpanet>
2022-04-28 16:24:58 +0200

    configuration webpack

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import './assets/styles/styles.scss';
+import './index.scss';
+
+const usersElement = document.querySelector('users');
+let username;
+let firstname;
+let lastname;
+let email;
+let phone;
+
+const createUsername = () =>{
+  
+}
